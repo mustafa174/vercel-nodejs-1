cb57bbf473517b67b08769bf601e21b7893c1157
CBMustafa <mustafa.hameed@crowdbotics.com>
2024-09-08 19:55:43 +0500

    x

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors"); // Import the cors package
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+// Use CORS middleware
+app.use(cors()); // Allows all origins by default
 
 // Define a port
 const port = process.env.PORT || 3000;
@@ -11,10 +15,17 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello, World from MUSTAFA");
 });
-
+const data = [
+  { id: 1, name: "Item 1", description: "Description of Item 1" },
+  { id: 2, name: "Item 2", description: "Description of Item 2" },
+  { id: 3, name: "Item 3", description: "Description of Item 3" },
+];
 // Define the /get-data route
 app.get("/get-data", (req, res) => {
-  res.send("Hello, World from MUSTAFA");
+  // Create an array of objects
+
+  // Send the array of objects as JSON
+  res.json(data);
 });
 
 // Define a POST endpoint
